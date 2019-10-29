3788dc8b19b36b05127c5a9dad820d32dcab4e99
Jimmy Dunn <jdunn@ischool.berkeley.edu>
2019-10-28 18:37:04 -0700

    Modified location predict

--- location_algo/location.py
+++ location_algo/location.py
@@ -1,77 +1,89 @@
-"""
-Basic model for location algorithm
-"""
+import joblib
 import json
+import re
 
 import numpy as np
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.metrics import accuracy_score
 
-# TODO:
-# evaluate the geotagged locations against self-reported locations in user bio
-# implement Ego Network
-
-"""
-Generate dummy data to build out pipeline
-"""
-LOCATIONS = ["San Francisco", "Chicago", "New York", "Atlanta", "Austin", "Portland", "Seattle", "Washington D.C."]
+def get_location_mode(locations):
+    """Takes a list of multiple locations and returns a str of the most frequent"""
+    return max(set(locations), key=locations.count)
 
 
-def random_time():
-    hour = np.random.randint(0, 24)
-    if hour < 10:
-        hour = "0"+str(hour)
-    minute = np.random.randint(0, 60)
-    if minute < 10:
-        minute = "0"+str(minute)
-    time_ = str(hour) + ":" + str(minute)
+def load_twitter_json(filepath):
+    """Reads input json and reshapes to a nested dictionary keyed by user"""
+    # instantiate output dictionary
+    results = dict()
 
-    return "2015-03-23T"+time_
+    with open(filepath, 'r') as fp:
+        data = json.load(fp)
+    time_regex = r"([0-1]?\d|2[0-3]):([0-5]?\d):([0-5]?\d)"
 
+    for location in data:
+        # location is a str representing place
+        # data[location] is a dict with keys (geocode, since_id, statuses)
+        # we want statuses, a nested dict of tweet_ids
+        for tweet_id in data[location]['statuses']:
+            tweet_timestamp = data[location]['statuses'][tweet_id]['created_at']
+            tweet_time = re.search(time_regex, tweet_timestamp).group()
+            user_id = str(data[location]['statuses'][tweet_id]['user']['id'])
 
-def create_dummy_data(num_users, num_tweets, locations=LOCATIONS):
-    users = []
-    for i in range(num_users):
-        # generate random tweets
-        user_tweets = list()
-        for j in range(num_tweets):
-            user_tweets.append({"tweet_id": str(j), "tweet_time": random_time()})
-        location = locations[np.random.randint(0, len(locations))]
-        users.append({"user_id": str(i), "location": location, "user_tweets": user_tweets})
-    dummy_json = json.dumps(users)
-    return json.loads(dummy_json)
+            if user_id not in results:
+                results[user_id] = dict()
+                # instantiate location as a list and append this location
+                results[user_id]['location'] = list()
+                results[user_id]['tweets'] = list()
+
+            # I suspect geocode is linked to tweet and not user, let's take majority as user location
+            # append the location
+            results[user_id]['location'].append(location)
+            results[user_id]['tweets'].append((tweet_id, tweet_time))
+
+    for user in results:
+        results[user]['location'] = get_location_mode(results[user]['location'])
+
+    return results
 
 
 def get_labels_and_features(data, fractional=True):
     labels = dict()
     user_tweet_times = dict()
 
-    for user in data:
-        id_ = user['user_id']
-        labels[id_] = user['location']
+    for user_id in data:
+        labels[user_id] = data[user_id]['location']
 
         hourly_buckets = {k: 0 for k in range(0, 24)}
-        for t in user['user_tweets']:
-            hour = int(t["tweet_time"].split("T")[1].split(':')[0])
+        for tweet_id, tweet_time in data[user_id]['tweets']:
+            hour = int(tweet_time.split(':')[0])
             hourly_buckets[hour] += 1
         if fractional:
             total_tweets = sum(hourly_buckets.values())
             hourly_buckets = {k: x/total_tweets for k, x in hourly_buckets.items()}
-        user_tweet_times[id_] = hourly_buckets
+        user_tweet_times[user_id] = hourly_buckets
 
     return labels, user_tweet_times
 
 
-def basic_model(num_users, num_tweets):
-    """Will remove parameters and replace with real data as input"""
-    # pull a set of users that have geolocation enabled
-    # hit the Search API for tweets from users in [50 metro areas]
-    # dummy simulates the complete data set
-    dummy_data = create_dummy_data(num_users, num_tweets)
+def preprocess_new_data(data, fractional=True):
+    """list of dict with created_at:'Mon Oct 28 15:04:51 +0000 2019'"""
+    hourly_buckets = {k: 0 for k in range(0, 24)}
+    for tweet in data:
+        hour = int(tweet['created_at'].split(" ")[3].split(':')[0])
+        hourly_buckets[hour] += 1
+        if fractional:
+            total_tweets = sum(hourly_buckets.values())
+            hourly_buckets = {k: x / total_tweets for k, x in hourly_buckets.items()}
+    return list(hourly_buckets.values())
+
+
+def basic_model(data):
+    """Testing end to end model"""
     # labels are user_id mapped to location names
     # features are user_id mapped to 0-24 hourly percentages of tweets
-    labels, features = get_labels_and_features(dummy_data)
+    labels, features = get_labels_and_features(data)
 
     # pipe into a naive bayes classifier
     y = np.array(list(labels.values()))
@@ -89,3 +101,16 @@ def basic_model(num_users, num_tweets):
     print(f"Cross validated average score over 5-Fold: {np.mean(cv_results['test_score'])}")
 
     return model
+
+def train():
+    data = load_twitter_json('./location.json')
+    model = basic_model(data)
+    joblib.dump(model, "./location_model.joblib")
+
+def predict_location(user_tweets):
+    """Takes in a list of user tweets with created_at and returns a str predicting the location"""
+    X = np.array(preprocess_new_data(user_tweets)).reshape(1, -1)  # single sample
+    model = joblib.load("./location_model.joblib")
+    return model.predict(X)
+
+    
