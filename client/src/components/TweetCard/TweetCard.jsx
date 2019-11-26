dfbfcb5972021a6e75e1009b7db749794c86856e
Sijie (Anne) Yu <yu.ann.eml@gmail.com>
2019-11-26 13:00:12 -0800

    Add icon.

--- client/src/components/TweetCard/TweetCard.jsx
+++ client/src/components/TweetCard/TweetCard.jsx
@@ -8,6 +8,8 @@ import { Typography, Link } from "@material-ui/core";
 import { grey } from "@material-ui/core/colors";
 import logo from './logo.png';
 import heart from './heart.png';
+import tweet from './tweet.png';
+import person from './people.png';
 
 const useStyles = makeStyles({
   card: {
@@ -25,15 +27,15 @@ const useStyles = makeStyles({
     marginBottom: 12
   },
   quotedTweet: {
-    //background: grey[300]
+    //background: grey[300],
     display: "block !important",
     margin: "5px 0 !important",
     padding: "7px 0 !important",
-    background: "#858585 !important",
-    color: "#fff !important",
+    background: "#ebebeb !important",
+    color: "#1f1f1f !important",
     textDecoration: "none !important",
     borderRadius: "13px !important",
-    boxShadow: "-1px -1px 2px #555 !important", 
+    boxShadow: "-1px -1px 2px #d6d6d6 !important", 
   },
   userInfo: {
     display: "flex",
@@ -57,6 +59,12 @@ const useStyles = makeStyles({
     padding: "5px !important",
     fontSize: 13,
     color: "#5c5c5c !important",
+    borderRadius: "13px !important",
+    textDecoration: "none !important",
+  },
+  icon: {
+    padding: "10 10px",
+    float: "left",
   }
 });
 
@@ -71,7 +79,7 @@ const TweetCard = props => {
     <Card className={classes.card}>
       <CardContent>
         <Typography variant="h6" gutterBottom>
-          <Link
+	  <Link
             href={
               "https://twitter.com/" +
               props.data.get("users").properties.screen_name +
@@ -85,35 +93,36 @@ const TweetCard = props => {
             gutterBottom
 	    className={classes.entryTitle}
           >
-	  @{props.data.get("users").properties.screen_name}:{" "}
-          </Link>
-	  <img src={logo} alt="Logo" align="right"/>
-	  <p className={classes.entrySubtitle}>
-            {new Date(
-              props.data.get("tweets").properties.created_at_date.toString()
-            ).toLocaleDateString() +" " + new Date(
-              props.data.get("tweets").properties.created_at_date.toString()
-            ).toLocaleTimeString() || "n/a creation"}
-	  </p>
-	</Typography>
+	    <img src={person} alt="Person" className={classes.icon}/>
+	    @{props.data.get("users").properties.screen_name}:{" "}
+            <img src={logo} alt="Logo" align="right"/>
+	  </Link>
+        </Typography>
         <Typography className={classes.quotedTweet} gutterBottom>
           {props.data.get("tweets").properties.text}
         </Typography>
         <div>
           <Typography variant="body2" component="p">
-            <img src={heart} alt="Heart" align="left"/>
-	    Retweets:{" "}
-            {!!props.data.get("tweets").properties.retweet_count
-              ? props.data.get("tweets").properties.retweet_count.toString()
-              : "n/a"}{" "}
-            | Favorites:{" "}
+	    <p className={classes.entrySubtitle}>
+	    <img src={heart} alt="Heart"/> Favorites:{" "}
             {!!props.data.get("tweets").properties.favorite_count
               ? props.data.get("tweets").properties.favorite_count.toString()
               : "n/a"}
+	      | Retweets:{" "}
+            {!!props.data.get("tweets").properties.retweet_count
+              ? props.data.get("tweets").properties.retweet_count.toString()
+              : "n/a"}{" "}
+	    | {new Date(
+              props.data.get("tweets").properties.created_at_date.toString()
+            ).toLocaleDateString() +" " + new Date(
+              props.data.get("tweets").properties.created_at_date.toString()
+            ).toLocaleTimeString() || "n/a creation"}  
+	    </p>
           </Typography>
         </div>
       </CardContent>
       <CardActions>
+	<img src={tweet} alt="Tweet" align="left"/>
         <Button size="small" onClick={handleClick}>
           View Insights
         </Button>
